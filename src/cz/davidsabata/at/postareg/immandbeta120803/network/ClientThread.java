1294bf7e273cfb1ff5d431e6c392700f823c53a0
david-sabata <david.n.sabata@gmail.com>
2012-10-14 01:34:40 +0200

    more client server

--- src/cz/davidsabata/at/postareg/immandbeta120803/network/ClientThread.java
+++ src/cz/davidsabata/at/postareg/immandbeta120803/network/ClientThread.java
@@ -3,20 +3,36 @@ package cz.davidsabata.at.postareg.immandbeta120803.network;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.List;
 
 import android.util.Log;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import cz.davidsabata.at.postareg.immandbeta120803.network.Message.Type;
+import cz.davidsabata.at.postareg.immandbeta120803.services.GameService;
+import cz.davidsabata.at.postareg.immandbeta120803.services.Player;
+import cz.davidsabata.at.postareg.immandbeta120803.services.Player.Role;
 
 public class ClientThread extends Thread {
 
-	Socket client;
+	public Socket client;
+
+	private final GameService gameService;
+
+	private final List<ClientThread> clThreads;
 
 
-	public ClientThread(Socket socket) {
+	public ClientThread(Socket socket, List<ClientThread> threads) {
+		gameService = GameService.getInstance();
+
+		clThreads = threads;
+
 		client = socket;
-		Log.d("CLIENT", client.toString());
+		Log.d("ClientThread", client.toString());
 	}
 
 
@@ -45,8 +61,56 @@ public class ClientThread extends Thread {
 
 	public void parseLine(String line) {
 		Gson g = new Gson();
-		Message msg = g.fromJson(line, Message.class);
-		Log.d("LOADED MSG", msg.toString());
+		try {
+			Message msg = g.fromJson(line, Message.class);
+			Log.d("ClientThread", msg.toString());
+
+			Player p = new Player();
+			p.isHost = false;
+			p.role = msg.playerRole == "AGENT" ? Role.AGENT : Role.GUARD;
+			p.nickname = msg.nickname;
+			p.macAddr = msg.playerMac;
+			p.lastKnownX = msg.lastX;
+			p.lastKnownY = msg.lastY;
+
+			// info o hraci pri skladani lidi na hru
+			if (gameService.getGameState() == cz.davidsabata.at.postareg.immandbeta120803.services.GameInfo.State.WAITING_FOR_CONNECTION && msg.type == Type.PREPARING) {
+				gameService.addPlayer(p);
+
+				// rozeslat info
+				sendMessage(msg, client);
+
+				return;
+			}
+
+
+
+
+		} catch (JsonSyntaxException e) {
+			Log.d("ClientThread-Error", e.toString());
+		}
+	}
+
+
+	private void sendMessage(Message msg, Socket except) {
+		synchronized (clThreads) {
+			try {
+				for (ClientThread cl : clThreads) {
+					if (cl.client.equals(except)) {
+						continue;
+					}
+
+					OutputStream os = cl.client.getOutputStream();
+					Gson g = new Gson();
+					String str = g.toJson(msg);
+					os.write(str.getBytes());
+					Log.d("ClientThread", "Msg sent");
+				}
+			} catch (IOException e) {
+				Log.e("ClientThread", "Error while sending message");
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
