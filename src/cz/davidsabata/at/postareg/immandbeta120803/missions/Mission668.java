891eda0d7ebe37fda6bf9249a4413461398f3802
david-sabata <david.n.sabata@gmail.com>
2012-10-13 17:37:59 +0200

    polishing camera

---
+++ src/cz/davidsabata/at/postareg/immandbeta120803/missions/Mission668.java
@@ -0,0 +1,15 @@
+package cz.davidsabata.at.postareg.immandbeta120803.missions;
+
+import cz.davidsabata.at.postareg.immandbeta120803.R;
+
+public class Mission668 extends BaseMission {
+
+	public Mission668() {
+		imageResId = R.drawable.match_668;
+		titleResId = R.string.mission668_title;
+		briefingResId = R.string.mission668_briefing;
+		locX = -1;
+		locY = -1;
+	}
+
+}
